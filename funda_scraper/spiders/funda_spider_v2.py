c131b8b73f8b9682afdf9ab0a60053bc6639427a
Jeroen Kortus <jeroenkortus@gmail.com>
2022-03-30 22:05:51 +0200

    Finished basics for v2

--- funda_scraper/spiders/funda_spider_v2.py
+++ funda_scraper/spiders/funda_spider_v2.py
@@ -1,99 +1,87 @@
-from fileinput import filename
-import scrapy
-from scrapyscript import Job, Processor
+from base64 import urlsafe_b64decode
 import re
+from scrapyscript import Job, Processor
+from scrapy.spiders import Spider
 import json
 import webbrowser
 import schedule
 import time
 
-class FundaSpider(scrapy.Spider):
-    name = 'fundaspiderv2'
+######## SCRAPY SPIDER FUNCTIONS #######
+class FundaSpider(Spider):
+    name = 'fundaspiderv3'
 
     def parse(self, response):
         for lstng in response.css(".search-result").getall():
-            yield {'text': lstng}
+            yield {'text': re.search('href=\"(.+?)\"', lstng).group(1)}
 
-def fill_listing_list(filename, spider):
-    """Takes a spider and an empty .json filename and fills it with all listings currently available"""
-    url_list = [link + str(i) for i in range(1,10) for link in \
-        ['https://www.funda.nl/koop/gemeente-den-bosch/200000-400000/dakterras/tuin/sorteer-datum-af/p',
-         'https://www.funda.nl/koop/gemeente-vught/200000-400000/dakterras/tuin/sorteer-datum-af/p',
-         'https://www.funda.nl/koop/gemeente-sint-michielsgestel/200000-400000/dakterras/tuin/sorteer-datum-af/p']]
-
-    FundaJob = Job(spider, start_urls = url_list)
+def run_spider(spider, url_list):
+    funda_job = Job(spider, start_urls = url_list)
     processor = Processor(settings=None)
-    fetched_listings = processor.run(FundaJob)
+    fetched_listings = processor.run(funda_job)
+    return fetched_listings
 
-    listings = []
-    for listing in fetched_listings:
-        listing_text = listing['text']
-        href_string = re.search('href=\"(.+?)\"', listing_text)
-        if href_string:
-            grouped_href_string = href_string.group(1)
-            listings.append(grouped_href_string)
+############ JSON FUNCTIONS ############
+def clear_json(filename):
+   f = open(filename, "r+")
+   f.seek(0)
+   f.truncate()
+   f.close()
 
-    json_string = json.dumps(listings)
-    with open(filename, 'w') as outfile:
-        outfile.write(json_string)
+def write_json(filename, listings):
+   json_string = json.dumps(listings)
+   with open(filename, 'w') as outfile:
+       outfile.write(json_string)
 
-def check_and_update_new_listings(filename, spider):
-    url_list = ['https://www.funda.nl/koop/gemeente-den-bosch/200000-400000/dakterras/tuin/sorteer-datum-af/',
-                'https://www.funda.nl/koop/gemeente-vught/200000-400000/dakterras/tuin/sorteer-datum-af/',
-                'https://www.funda.nl/koop/gemeente-sint-michielsgestel/200000-400000/dakterras/tuin/sorteer-datum-af/']
-    FundaJob = Job(spider, start_urls = url_list)
-    processor = Processor(settings=None)
-    fetched_listings = processor.run(FundaJob)
-
+def read_json(filename):
     f = open(filename)
-    old_listings = json.load(f)
+    saved_listings = json.load(f)
     f.close()
+    return saved_listings
 
-    listings = []
-    for listing in fetched_listings:
-        listing_text = listing['text']
-        href_string = re.search('href=\"(.+?)\"', listing_text)
-        if href_string:
-            grouped_href_string = href_string.group(1)
-            listings.append(grouped_href_string)
+##### LISTING COMPARISON FUNCTIONS #####
+def check_new_listings(spider_output, all_listings, open_link=True):
+    for lstng in spider_output:
+        listing = lstng['text']
+        if listing not in all_listings:
+            print("adding new listing!!!: ", listing)
+            all_listings.append(listing)
+            base_html = "https://www.funda.nl"
+            if open_link:
+                webbrowser.open(base_html + listing)
+  
+    return all_listings
 
-    print("#######################################\n###########checking listings###########\n#######################################")
-    
-    for listing in listings:
-        if listing not in old_listings:
-            print("adding new listing: ", listing)
-            old_listings.append(listing)
-            #base_html = "https://www.funda.nl"
-            #webbrowser.open(base_html + listing)
-    
 
-    # f = open(filename)
-    # f.seek(0) 
-    # f.truncate(0) 
-    # f.close()
+def periodic_checker(database, url_list):
+    saved_listings = read_json(database)
 
-    json_string = json.dumps(old_listings)
-    with open(filename, 'w') as outfile:
-        outfile.write(json_string)
+    # Check if database is empty. fill without opening links if so
+    open_links = False if not saved_listings else True
 
-def check_database_empty(filename):
-    f = open(filename)
-    old_listings = json.load(f)
-    f.close()
-    if not old_listings:
-        return True
+    # Run spider and check for new listings
+    fetched_listings = run_spider(FundaSpider, url_list)
+    all_listings = check_new_listings(fetched_listings, saved_listings, open_links)
+
+    # Clear database and write new complete list of listings
+    clear_json(database)
+    write_json(database, all_listings)
+
+
+############### VARIABLES ##############
+one_loop = True
+database = "listing_database.json"
+url_list = ['https://www.funda.nl/koop/gemeente-den-bosch/200000-400000/dakterras/tuin/sorteer-datum-af/',
+            'https://www.funda.nl/koop/gemeente-vught/200000-400000/dakterras/tuin/sorteer-datum-af/']
+
+############## MAIN LOOP ###############
+if __name__ == "__main__":
+
+    if one_loop:
+        periodic_checker(database, url_list)
     else:
-        return False
+        schedule.every(15).minutes.do(periodic_checker, database = database, url_list = url_list)
 
-
-filename = "listing_database_v2.json"
-spider = FundaSpider
-
-if check_database_empty(filename):
-    fill_listing_list(filename, spider)
-
-schedule.every(10).seconds.do(check_and_update_new_listings,filename, spider)
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
