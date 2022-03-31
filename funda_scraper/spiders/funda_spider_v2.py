69238636ecda70b4b51948803590ddbc7f0686cd
Jeroen Kortus <jeroenkortus@gmail.com>
2022-03-31 23:43:39 +0200

    Added notification to v2

--- funda_scraper/spiders/funda_spider_v2.py
+++ funda_scraper/spiders/funda_spider_v2.py
@@ -6,6 +6,11 @@ import json
 import webbrowser
 import schedule
 import time
+import requests
+
+import sys
+sys.path.insert(0, '/Users/jk/data_science/funda_scraper/')
+import config
 
 ######## SCRAPY SPIDER FUNCTIONS #######
 class FundaSpider(Spider):
@@ -39,49 +44,74 @@ def read_json(filename):
     f.close()
     return saved_listings
 
-##### LISTING COMPARISON FUNCTIONS #####
-def check_new_listings(spider_output, all_listings, open_link=True):
+###### PUSH NOTIFICATION FUNCTION ######
+def pushbullet_noti(title, body, token):
+
+    # Make a dictionary that includes, title and body
+    msg = {"type": "note", "title": title, "body": body}
+
+    # Sent a posts request
+    resp = requests.post('https://api.pushbullet.com/v2/pushes',
+                         data=json.dumps(msg),
+                         headers={'Authorization': 'Bearer ' + token,
+                                  'Content-Type': 'application/json'})
+
+    if resp.status_code != 200:  # Check if fort message send with the help of status code
+        raise Exception('Error', resp.status_code)
+    else:
+        print('Message sent!')
+
+###### LISTING COMPARISON FUNCTION #####
+def check_new_listings(spider_output, all_listings):
+    new_listings = []
     for lstng in spider_output:
         listing = lstng['text']
         if listing not in all_listings:
             print("adding new listing!!!: ", listing)
             all_listings.append(listing)
             base_html = "https://www.funda.nl"
-            if open_link:
-                webbrowser.open(base_html + listing)
+            new_listings.append(base_html + listing)
   
-    return all_listings
+    return all_listings, new_listings
 
-
-def periodic_checker(database, url_list):
+############ MAIN FUNCTION #############
+def periodic_checker(database, url_list, token, open_links):
     saved_listings = read_json(database)
 
-    # Check if database is empty. fill without opening links if so
-    open_links = False if not saved_listings else True
+    # overwrtie if database is empty.
+    open_links = False if not saved_listings else open_links
 
     # Run spider and check for new listings
     fetched_listings = run_spider(FundaSpider, url_list)
-    all_listings = check_new_listings(fetched_listings, saved_listings, open_links)
+    all_listings, new_listings = check_new_listings(fetched_listings, saved_listings)
+
+    for listing in new_listings:
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+        pushbullet_noti("New listing found! ({})".format(current_time), listing, token)
+        if open_links:
+            webbrowser.open(listing)
 
     # Clear database and write new complete list of listings
     clear_json(database)
     write_json(database, all_listings)
 
-
 ############### VARIABLES ##############
-one_loop = True
+one_loop = False
+open_links = False
 database = "listing_database.json"
 url_list = ['https://www.funda.nl/koop/gemeente-den-bosch/200000-400000/dakterras/tuin/sorteer-datum-af/',
             'https://www.funda.nl/koop/gemeente-vught/200000-400000/dakterras/tuin/sorteer-datum-af/']
+token = config.PUSHBULLET_TOKEN
 
 ############## MAIN LOOP ###############
 if __name__ == "__main__":
 
     if one_loop:
-        periodic_checker(database, url_list)
+        periodic_checker(database, url_list, token)
     else:
-        schedule.every(15).minutes.do(periodic_checker, database = database, url_list = url_list)
+        schedule.every(15).minutes.do(periodic_checker, database = database, url_list = url_list, token = token)
 
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
