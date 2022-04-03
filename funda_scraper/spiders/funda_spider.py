58b699b02800d9c720c6e12f3895248691da788e
Jeroen Kortus <jeroenkortus@gmail.com>
2022-04-03 23:09:41 +0200

    preliminary work to use ItemLoader

--- funda_scraper/spiders/funda_spider.py
+++ funda_scraper/spiders/funda_spider.py
@@ -6,6 +6,13 @@ class FundaSpider(Spider):
     name = 'funda_spider'
 
     def parse(self, response):
-        for lstng in response.css(".search-result").getall():
+        listings = response.css(".search-result").getall()
+        for lstng in listings:
             yield {'text': re.search('href=\"(.+?)\"', lstng).group(1)}
-
+            #'street_name': lstng.css(".search-result__header-title.fd-m-none::text").getall() - pp remove \r\n
+            #'postal_code': lstng.css(".search-result__header-subtitle.fd-m-none::text").getall() - pp remove \r\n
+            #'price': lstng.css(".search-result-price::text").get() - pp turn into int
+            #'living_space': lstng.css("ul.search-result-kenmerken span ::text").get()
+            #'plot_size': lstng.css("ul.search-result-kenmerken span ::text").getall() - pp get [1]
+            #'nr_of_rooms': lstng.css("ul.search-result-kenmerken li ::text").getall() - pp get [-1]
+            #'url': lstng.css("div.search-result__header-title-col a::attr(href)").get() - pp add base url
