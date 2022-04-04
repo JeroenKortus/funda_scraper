38a699064c282ed6e445353a6d14087383c04576
Jeroen Kortus <jeroenkortus@gmail.com>
2022-04-04 20:35:00 +0200

    added ItemLoader and improved notification

--- funda_scraper/spiders/funda_spider.py
+++ funda_scraper/spiders/funda_spider.py
@@ -1,18 +1,29 @@
-from base64 import urlsafe_b64decode
-import re
 from scrapy.spiders import Spider
+from scrapy.loader import ItemLoader
+from funda_scraper.items import FundaScraperItem
 
+# define spider
 class FundaSpider(Spider):
+
     name = 'funda_spider'
 
     def parse(self, response):
-        listings = response.css(".search-result").getall()
+
+        # make spider get search-result from webpages
+        listings = response.css('li.search-result')
         for lstng in listings:
-            yield {'text': re.search('href=\"(.+?)\"', lstng).group(1)}
-            #'street_name': lstng.css(".search-result__header-title.fd-m-none::text").getall() - pp remove \r\n
-            #'postal_code': lstng.css(".search-result__header-subtitle.fd-m-none::text").getall() - pp remove \r\n
-            #'price': lstng.css(".search-result-price::text").get() - pp turn into int
-            #'living_space': lstng.css("ul.search-result-kenmerken span ::text").get()
-            #'plot_size': lstng.css("ul.search-result-kenmerken span ::text").getall() - pp get [1]
-            #'nr_of_rooms': lstng.css("ul.search-result-kenmerken li ::text").getall() - pp get [-1]
-            #'url': lstng.css("div.search-result__header-title-col a::attr(href)").get() - pp add base url
+
+            # initiate item loader for spider output
+            loader = ItemLoader(item=FundaScraperItem(), selector=lstng)
+            
+            # add relevant items to loader
+            loader.add_css('street_name', '.search-result__header-title.fd-m-none::text')
+            loader.add_css('postal_code', '.search-result__header-subtitle.fd-m-none::text')
+            loader.add_css('price', '.search-result-price::text')
+            loader.add_css('living_space', 'ul.search-result-kenmerken span ::text')
+            loader.add_css('plot_size', 'ul.search-result-kenmerken span ::text')
+            loader.add_css('nr_of_rooms', 'ul.search-result-kenmerken li ::text')
+            loader.add_css('url', 'div.search-result__header-title-col a::attr(href)')
+            
+            # yield result
+            yield loader.load_item()
